d74f13917214ff79f496ed9a7a2143c9e45dd704
Gabriel Yamashita <gabriel.yamashita01@gmailcom>
2021-08-25 14:32:46 -0300

    atualizando powershell

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,27 +1,29 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-PoshPrompt -Theme material
 
+# Função para ir para um Diretório específico:
+function path { Set-Location "C:\Users" }
 
-
-function home { Set-Location "C:\Users\gabri" }
-function yacomp { Set-Location "C:\Users\gabri\Documents\YACOMP" }
-function html { Set-Location "C:\Users\gabri\Documents\YACOMP\html" }
-function py { Set-Location "C:\Users\gabri\Documents\YACOMP\python" }
-
-
-
+# Cria nova pasta e vai para ela:
 function new { mkdir $args ; Set-Location ".\$args" ; Clear-Host }
+
+# Volta para o diretório anterior:
 function back { Set-Location ".." ; Clear-Host }
 
+# Funções do github:
 
-
-$Token = 'USER:TOKEN'
-$Base64Token = [System.Convert]::ToBase64String([char[]]$Token);
+# Settings
+$USER = 'USER' # Substituir USER pelo user do github
+$TOKEN = 'TOKEN' # Substituir TOKEN pelo token gerado.
+$Pass = "$($USER):$($TOKEN)"
+$Base64Token = [System.Convert]::ToBase64String([char[]]$Pass);
 $Headers = @{
    Authorization = 'Basic {0}' -f $Base64Token;
 };
 
-function create { git init ; Invoke-RestMethod -Headers $Headers -Uri https://api.github.com/user/repos -Body "{""name"":""$($args[0])""}" -Method Post ; git remote add origin git@github.com:GabrielYamashita/$($args[0]).git ; git add . ; git commit -m "Initial Commit" ;  git push --set-upstream origin master ; Clear-Host ; Write-Host "New Repository ($($args[0])) Created!" -ForegroundColor green }
+# Cria um repositório do zero e adiciona tuda a ele:
+function create { git init ; Invoke-RestMethod -Headers $Headers -Uri https://api.github.com/user/repos -Body "{""name"":""$($args[0])""}" -Method Post ; git remote add origin git@github.com:$USER/$($args[0]).git ; git add . ; git commit -m "Initial Commit" ;  git push --set-upstream origin master ; Clear-Host ; Write-Host "New Repository ($($args[0])) Created!" -ForegroundColor green }
+
+# Git Status:
 function status { git status }
+
+# Commit com a mensagem desejada:
 function commit { git add . ; git commit -m "$args" ; git push ; Write-Host "Successful Commit ($args)!" -ForegroundColor green }
